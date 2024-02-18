fc4a7029ea6764fd330dc7296743f6d57d3e20a9
Akshith268 <akshith268@gmail.com>
2024-02-18 22:16:25 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request,session,jsonify,render_template_string
+from flask import redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+import bcrypt
+from flask import flash
+import pandas as pd
+from wtforms import IntegerField, SelectField
+from wtforms.validators import InputRequired
+from flask_mail import Mail, Message
+import random
+import os
+import csv
+
+#Flask App
+#------------------------------------------
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app = Flask(__name__)
+app.secret_key = "778031a659c117f6ab82986676e24271"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MediSearch.db'
+db = SQLAlchemy(app)
